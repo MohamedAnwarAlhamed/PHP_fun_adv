372df752d73a54091f839dfad2b5a166c83534cf
Mohamed_Anwar <alhamed202024@gmail.com>
2023-02-17 17:38:28 +0300

    oop 7

---
+++ access modifier and encapsulation.php
@@ -0,0 +1,60 @@
+// there are 3 types of modifier
+
+->public,
+->private,
+->protected
+
+
+//access modifier syntax never can put before classs
+
+//private only access in class
+
+//to access  private method need getter & setter method
+
+<?php
+
+
+// private method can access by another method
+class House{
+
+	private $road;
+	private $owner;
+
+	//getter & setter
+
+	//get method return/get the value & set method set the actual value
+	//have to return the value
+	function getroad(){
+		return $this->road;
+	}
+
+	function setRoad($road) :string{
+		$this->road=$road;
+		return $road;
+	}
+	function getOwner(){
+		return $this->owner;
+	}
+
+	function setOwner($owner) :string{
+		$this->owner=$owner;
+		return $owner;
+	}
+
+	function area(){
+		echo "welcome to NoyaBazar";
+	}
+
+	function moholla(){
+		echo " hlw from chowdhuryPara";
+	}
+}
+
+	
+$house = new House();
+
+//accessing the private modifier
+$house->setRoad("twelve");
+$house->setOwner("Ferdush");
+$house->area();
+$house->moholla();
